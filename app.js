c6e360ebc793d1df46d056d74818c9d4524de6a5
DavidSaupe <david.saupe@gmail.com>
2022-03-29 20:25:06 +0200

    merge

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
 let centerx = window.innerWidth / 2;
 let centery = window.innerHeight / 2;
-const earthRadius = 93;
+const marsSizeMultiplier = 2;
+const earthSizeMultiplier = 2;
+const mercurySizeMultiplier = 3;
 
+// earth - settings
+const earthRadius = 93;
+const earthSize = 12.7;
+
+
+    //earth-ellipse
 const earthEllipse = document.querySelector(".earthEllipse");
 earthEllipse.style.left = centerx - earthRadius * 2 + "px";
-earthEllipse.style.top = centery - earthRadius  + "px";
+earthEllipse.style.top = centery + (earthSize * earthSizeMultiplier / 2 ) - earthRadius  + "px";
+earthEllipse.style.width = earthRadius * 4 + "px";
+earthEllipse.style.height = earthRadius * 2 + "px";
 
-console.log(earthEllipse)
+    //earth
+const earth = document.querySelector(".earth");
+earth.style.width = earthSize * earthSizeMultiplier + "px";
 
+
+// mars - settings
+const marsRadius = 141;
+const marsSize = 6.7;
+
+
+    //mars-ellipse
+const marsEllipse = document.querySelector(".marsEllipse");
+marsEllipse.style.left = centerx - marsRadius * 2 + "px";
+marsEllipse.style.top = centery + (marsSize * marsSizeMultiplier / 2 ) - marsRadius  + "px";
+marsEllipse.style.width = marsRadius * 4 + "px";
+marsEllipse.style.height = marsRadius * 2 + "px";
+
+    //mars 
+const mars = document.querySelector(".mars");
+mars.style.width = marsSize * marsSizeMultiplier + "px";
+
+
+// mercury - settings
+const mercuryRadius = 57;
+const mercurySize = 4.8;
+
+
+    //mercury-ellipse
+const mercuryEllipse = document.querySelector(".mercuryEllipse");
+mercuryEllipse.style.left = centerx - mercuryRadius * 2 + "px";
+mercuryEllipse.style.top = centery + (mercurySize * mercurySizeMultiplier / 2 ) - mercuryRadius  + "px";
+mercuryEllipse.style.width = mercuryRadius * 4 + "px";
+mercuryEllipse.style.height = mercuryRadius * 2 + "px";
+
+    //mercury 
+const mercury = document.querySelector(".mercury");
+mercury.style.width = mercurySize * mercurySizeMultiplier + "px";
